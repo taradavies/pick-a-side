49420317a54ac30bcf08720fd58d686a087329dd
Tara Davies <tarawafflebear@gmail.com>
2022-06-25 20:05:17 +0800

    pattern cube changes colors resembling sequence

--- Assets/Scripts/PatternCube.cs
+++ Assets/Scripts/PatternCube.cs
@@ -1,21 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PatternCube : MonoBehaviour
 {
-    [SerializeField] Color[] _possibleColors;
 
+    [SerializeField] Color[] _possibleColors;
+    [SerializeField] float _timeBetweenColorsDelay;
+
+    public List<Color> _activeColorSequence = new List<Color>();
     MeshRenderer _meshRenderer;
+    int _positionInColorSequence;
+    bool _showingSequenceColor = false;
 
     void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
     }
 
+    void Update()
+    {
+        if (_showingSequenceColor) {return;}
+
+        StartCoroutine(AddRandomColorToSequence());
+
+    }
+
     void Start()
     {
-        int randomColorIndex = UnityEngine.Random.Range(0, 6);
-        _meshRenderer.material.color = _possibleColors[randomColorIndex];
+        _activeColorSequence.Clear();
+    }
+
+    IEnumerator AddRandomColorToSequence()
+    {
+        Debug.Log("Started Coroutine");
+
+        _showingSequenceColor = true;
+
+        Color newPatternCubeColor = GetRandomColor();
+
+        _meshRenderer.material.color = newPatternCubeColor;
+
+        _activeColorSequence.Add(newPatternCubeColor);
+        _positionInColorSequence++;
+
+        yield return new WaitForSeconds(_timeBetweenColorsDelay);
+        _showingSequenceColor = false;
+        Debug.Log("Ended Couroutine");
+    }
+
+    Color GetRandomColor()
+    {
+        int randomColorIndex = UnityEngine.Random.Range(0, _possibleColors.Length);
+        return _possibleColors[randomColorIndex];
     }
 }
